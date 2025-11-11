e71a8187525c6448b7c78621f2bea56ed60a6006
dangtretrau <155899086+dangtretrau@users.noreply.github.com>
2025-11-11 18:59:30 +0700

    Update script.js

--- bio1/other/script.js
+++ bio1/other/script.js
@@ -4,6 +4,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const welcomeOverlay = document.getElementById('welcome-overlay');
     const bgVideo = document.getElementById('bg-video');
     const bgAudio = document.getElementById('bg-audio');
+    const bioContainer = document.querySelector('.bio-container'); 
+    function getRandomColor() {
+        let color = Math.floor(Math.random()*16777215).toString(16);
+        while(color.length < 6) {
+            color = "0" + color;
+        }
+        return "#" + color;
+    }
+
+    if (bioContainer) {
+        bioContainer.addEventListener('mouseenter', () => {
+            const neonColor = getRandomColor();
+            bioContainer.style.setProperty('--neon-color', neonColor);
+        });
+    }
 
     enterBtn.addEventListener('click', () => {
         
